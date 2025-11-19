9a2fbea2a6e49a8527403c614a6d552531012124
Len-mad <lenin.madhu@gmail.com>
2025-11-19 18:14:39 +0530

    initial commit

---
+++ greetings.py
@@ -0,0 +1,7 @@
+#Personalized greetings
+#get inputs
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+last_name=last_name+"!"
+#concatinate into full name
+print("Hello,",first_name,last_name,"welcome to the python program.",sep=" ")
