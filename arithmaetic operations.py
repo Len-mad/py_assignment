f7a36d96de0a1bc4d4cb76ed1b108743aa27a916
Len-mad <lenin.madhu@gmail.com>
2025-11-19 18:21:51 +0530

    initial commit

---
+++ arithmaetic operations.py
@@ -0,0 +1,9 @@
+#get inputs
+num1=int(input("Enter a number: "))
+num2=int(input("Enter another number: "))
+
+#perform and print arithmetic operations
+print("Addition of ",num1,"+",num2,"is:",num1+num2)
+print("subtraction of ",num1,"-",num2,"is:",num1-num2)
+print("multiplication of ",num1,"*",num2,"is:",num1*num2)
+print("division of ",num1,"/",num2,"is:",num1/num2)
